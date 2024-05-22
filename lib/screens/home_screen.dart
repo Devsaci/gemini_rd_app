6390f90a793f823a790488a58c5899301635fe0b
saci zakaria <sacideveloppeur@gmail.com>
2024-05-22 19:07:53 +0200

    final List<Widget> _screens

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'chat_history_screen.dart';
+import 'chat_screen.dart';
+import 'profile_screen.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -8,6 +12,12 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // list of screens
+  final List<Widget> _screens = [
+    const ChatHistoryScreen(),
+    const ChatScreen(),
+    const ProfileScreen(),
+  ];
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
