bfba1f598b8f6877dcf5fb276f30cc37c8597926
saci zakaria <sacideveloppeur@gmail.com>
2024-05-22 18:46:52 +0200

    class ChatScreen extends StatefulWidget

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
