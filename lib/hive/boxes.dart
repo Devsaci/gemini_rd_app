eaaa78dbec754e051994ab8139cc6b9b2a6989a3
saci zakaria <sacideveloppeur@gmail.com>
2024-05-23 23:14:54 +0200

    getUser()

--- lib/hive/boxes.dart
+++ lib/hive/boxes.dart
@@ -1,9 +1,13 @@
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'chat_history.dart';
+import 'user_model.dart';
 
 class Boxes {
   // get the caht history box
   static Box<ChatHistory> getChatHistory() =>
       Hive.box<ChatHistory>('chat_history');
+
+  // get user box
+  static Box<UserModel> getUser() => Hive.box<UserModel>('user');
 }
