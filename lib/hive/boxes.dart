1fb0c1f7d48f864abf89917fc745f585cb46400c
saci zakaria <sacideveloppeur@gmail.com>
2024-05-23 23:13:51 +0200

    getChatHistory()

--- lib/hive/boxes.dart
+++ lib/hive/boxes.dart
@@ -1,1 +1,9 @@
-class Boxes {}
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'chat_history.dart';
+
+class Boxes {
+  // get the caht history box
+  static Box<ChatHistory> getChatHistory() =>
+      Hive.box<ChatHistory>('chat_history');
+}
