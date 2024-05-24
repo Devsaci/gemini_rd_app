c7e5e217c67729053413d9006b79f5bbac16703d
saci zakaria <sacideveloppeur@gmail.com>
2024-05-24 10:25:47 +0200

    Constants.chatHistoryBox

--- lib/hive/boxes.dart
+++ lib/hive/boxes.dart
@@ -1,3 +1,4 @@
+import 'package:gemini_rd_app/constants.dart';
 import 'package:gemini_rd_app/hive/chat_history.dart';
 import 'package:gemini_rd_app/hive/settings.dart';
 import 'package:gemini_rd_app/hive/user_model.dart';
@@ -6,7 +7,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 class Boxes {
   // get the caht history box
   static Box<ChatHistory> getChatHistory() =>
-      Hive.box<ChatHistory>('chat_history');
+      Hive.box<ChatHistory>(Constants.chatHistoryBox);
 
   // get user box
   static Box<UserModel> getUser() => Hive.box<UserModel>('user');
