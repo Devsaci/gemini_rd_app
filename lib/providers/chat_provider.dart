d014754a494ed1883b3317dcfe3b8c366c1ec862
saci zakaria <sacideveloppeur@gmail.com>
2024-05-24 10:41:06 +0200

    static initHive() async {}

--- lib/providers/chat_provider.dart
+++ lib/providers/chat_provider.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ChatProvider extends ChangeNotifier {}
+class ChatProvider extends ChangeNotifier {
+  // Init Hive boxes
+  static initHive() async {}
+}
