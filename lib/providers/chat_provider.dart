3d90082318fdae10544d854c6b228d76d4fa7976
saci zakaria <sacideveloppeur@gmail.com>
2024-05-24 10:51:34 +0200

    Hive.init

--- lib/providers/chat_provider.dart
+++ lib/providers/chat_provider.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart' as path;
 
 class ChatProvider extends ChangeNotifier {
   // Init Hive boxe
   static initHive() async {
     final dir = await path.getApplicationDocumentsDirectory();
+    Hive.init(dir.path);
+
     //await Hive.initFlutter();
   }
 }
