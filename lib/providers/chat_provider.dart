1fd55549ff1c5733061e1cef90a6fdd3555faf5f
saci zakaria <sacideveloppeur@gmail.com>
2024-05-24 11:03:13 +0200

    registerAdapter(ChatHistoryAdapter())

--- lib/providers/chat_provider.dart
+++ lib/providers/chat_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gemini_rd_app/constants.dart';
+import 'package:gemini_rd_app/hive/chat_history.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart' as path;
 
@@ -11,6 +12,8 @@ class ChatProvider extends ChangeNotifier {
     await Hive.initFlutter(Constants.geminiDB);
 
     //REGISTER ADAPTERS
-    if (!Hive.isAdapterRegistered(0)) {}
+    if (!Hive.isAdapterRegistered(0)) {
+      Hive.registerAdapter(ChatHistoryAdapter());
+    }
   }
 }
