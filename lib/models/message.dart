cbf71478c172e75fe227d7e8a46bf8086bc5bd8d
saci zakaria <sacideveloppeur@gmail.com>
2024-05-26 21:45:16 +0200

    Map<String, dynamic> toMap()

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -14,19 +14,31 @@ class Message {
     required this.imagesUrls,
     required this.timeSent,
   });
+
+  // toMap
+  Map<String, dynamic> toMap() {
+    return {
+      'messageId': messageId,
+      'chatId': chatId,
+      'role': role.toString(),
+      'message': message.toString(),
+      'imagesUrls': imagesUrls,
+      'timeSent': timeSent.toString(),
+    };
+  }
 }
 
 // toMap
-Map<String, dynamic> toMap(Message message) {
-  return {
-    'messageId': message.messageId,
-    'chatId': message.chatId,
-    'role': message.role.toString(),
-    'message': message.message.toString(),
-    'imagesUrls': message.imagesUrls,
-    'timeSent': message.timeSent.toString(),
-  };
-}
+// Map<String, dynamic> toMap(Message message) {
+//   return {
+//     'messageId': message.messageId,
+//     'chatId': message.chatId,
+//     'role': message.role.toString(),
+//     'message': message.message.toString(),
+//     'imagesUrls': message.imagesUrls,
+//     'timeSent': message.timeSent.toString(),
+//   };
+// }
 
 // fromMap
 
