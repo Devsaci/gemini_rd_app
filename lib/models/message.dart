a8cdbaa7552ad345dec5c5e1694013103b78ad05
saci zakaria <sacideveloppeur@gmail.com>
2024-05-26 21:28:59 +0200

    Role role;

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -1,8 +1,13 @@
 class Message {
   String messageId;
   String chatId;
+  Role role;
+
   Message({
     required this.messageId,
     required this.chatId,
+    required this.role,
   });
 }
+
+class Role {}
