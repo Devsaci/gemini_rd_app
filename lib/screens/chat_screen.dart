6fb0566af1f59ad7578e2d684d145da4c06471b0
saci zakaria <sacideveloppeur@gmail.com>
2024-05-27 20:48:25 +0200

    Consumer<ChatProvider>

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gemini_rd_app/providers/chat_provider.dart';
+import 'package:provider/provider.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -10,10 +12,13 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Chat Screen"),
-      ),
-    );
+    return Consumer<ChatProvider>(
+        builder: (context, ChatProvider chatProvider, child) {
+      return const Scaffold(
+        body: Center(
+          child: Text("Chat Screen"),
+        ),
+      );
+    });
   }
 }
