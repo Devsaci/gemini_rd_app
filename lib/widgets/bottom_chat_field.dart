a8977518768c6c13ce144565a41bf396c0af8751
saci zakaria <sacideveloppeur@gmail.com>
2024-05-29 20:25:22 +0200

    onSubmitted:

--- lib/widgets/bottom_chat_field.dart
+++ lib/widgets/bottom_chat_field.dart
@@ -33,6 +33,7 @@ class _BottomChatFieldState extends State<BottomChatField> {
               focusNode: textFieldFocus,
               controller: texteController,
               textInputAction: TextInputAction.send,
+              onSubmitted: (value) {},
               decoration: const InputDecoration(
                 hintText: 'Type a message',
                 border: InputBorder.none,
