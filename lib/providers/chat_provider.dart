b79c5cbf1b7f3477cd5ca8666a92f894d0d12938
saci zakaria <sacideveloppeur@gmail.com>
2024-06-11 21:11:51 +0200

    final imageFutures =

--- lib/providers/chat_provider.dart
+++ lib/providers/chat_provider.dart
@@ -224,7 +224,7 @@ class ChatProvider extends ChangeNotifier {
       return Content.text(message);
     } else {
       // generate image from text and image input
-      _imagesFileList
+      final imageFutures = _imagesFileList
           ?.map((imageFile) => imageFile.readAsBytes())
           .toList(growable: false);
       return Content.text('message');
