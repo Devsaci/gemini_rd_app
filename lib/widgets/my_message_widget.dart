bf923b2f924f07b64a7ceb9d7256db3f1b5c0de1
saci zakaria <sacideveloppeur@gmail.com>
2024-06-16 22:38:38 +0200

    final Message message;

--- lib/widgets/my_message_widget.dart
+++ lib/widgets/my_message_widget.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:gemini_rd_app/models/message.dart';
 
 class MyMessageWidget extends StatelessWidget {
-  const MyMessageWidget({super.key});
+  const MyMessageWidget({
+    super.key,
+    required this.message,
+  });
+  final Message message;
 
   @override
   Widget build(BuildContext context) {
