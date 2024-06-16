070c2a6a5f9ff7a73124f230bc7eb1a665a0b219
saci zakaria <sacideveloppeur@gmail.com>
2024-06-16 23:18:04 +0200

    class AssistantMessageWidget

--- lib/widgets/assistance_message_widget.dart
+++ lib/widgets/assistance_message_widget.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class AssistantMessageWidget extends StatelessWidget {
+  const AssistantMessageWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
