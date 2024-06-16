d625c50a6ae323aaa50a73dad1e1cbf5b648770a
saci zakaria <sacideveloppeur@gmail.com>
2024-06-16 22:55:57 +0200

    colorScheme.primaryContainer

--- lib/widgets/my_message_widget.dart
+++ lib/widgets/my_message_widget.dart
@@ -19,7 +19,7 @@ class MyMessageWidget extends StatelessWidget {
           maxWidth: MediaQuery.of(context).size.width * 0.7,
         ),
         decoration: BoxDecoration(
-          color: Theme.of(context).primaryColor,
+          color: Theme.of(context).colorScheme.primaryContainer,
         ),
       ),
     );
