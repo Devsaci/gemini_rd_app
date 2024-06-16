bb63d5c396075e8075f19f9bb6bfe91e7d1b3ab2
saci zakaria <sacideveloppeur@gmail.com>
2024-06-16 22:35:39 +0200

    class MyMessageWidget extends StatelessWidget

--- lib/widgets/my_message_widget.dart
+++ lib/widgets/my_message_widget.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class MyMessageWidget extends StatelessWidget {
+  const MyMessageWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
